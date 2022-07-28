cc9ac779f9e241f08d5823c96d599cbff57ee29c
jayaprasad356 <jayaprasad356@gmail.com>
2022-07-28 22:00:31 +0530

    updated

--- includes/crud.php
+++ includes/crud.php
@@ -5,7 +5,7 @@
  * @Package Database
  */
 
-define('DOMAIN_URL', 'http://localhost/ecomm/'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
+define('DOMAIN_URL', 'http://onlukr.greymatterworks.in/'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
 define('ALLOW_MODIFICATION', 1);
 define('JWT_SECRET_KEY', 'black_kite'); /* chnage to your secret key here set it to anything you like*/
 
@@ -19,9 +19,9 @@ class Database
      */
 
     private $db_host = "localhost";  // Change as required
-    private $db_user = "root";  // Change as required
-    private $db_pass = "";  // Change as required
-    private $db_name = "ecomm";    // Change as required
+    private $db_user = "u743445510_onlukr";  // Change as required
+    private $db_pass = "Onlukr@2022";  // Change as required
+    private $db_name = "u743445510_onlukr";    // Change as required
 
     /*
      * Extra variables that are required by other function such as boolean con variable
